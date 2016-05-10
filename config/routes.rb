2747c8b739c4d28c46754f89e8e6bba78d4932db
florianjosefreheis <florianjosefreheis@gmail.com>
2016-05-10 10:56:39 +1000

    setup api controller and routes

--- config/routes.rb
+++ config/routes.rb
@@ -1,5 +1,9 @@
 Rails.application.routes.draw do
 
+  resources :outfits
+  resources :occasions
 
-  
+  get '/api/outfits' => 'api/outfits#index'
+  get 'api/outfit/:id' => 'api/outfit#show'
+
 end
