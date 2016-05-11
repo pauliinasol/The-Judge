e5e1cdf61c57fb0de1a4f0d955e53dd2aec8da44
florianjosefreheis <florianjosefreheis@gmail.com>
2016-05-11 10:48:29 +1000

    creating seeds in seed file for database

--- db/seeds.rb
+++ db/seeds.rb
@@ -5,3 +5,53 @@
 #
 #   cities = City.create([{ name: 'Chicago' }, { name: 'Copenhagen' }])
 #   Mayor.create(name: 'Emanuel', city: cities.first)
+
+# Create users
+user = User.new
+user.user_name = 'Harry'
+user.email = '123@ga.com'
+user.password_digest = '123'
+user.save
+
+user = User.new
+user.user_name = 'Flo'
+user.email = '123@ga.com'
+user.password_digest = '123'
+user.save
+
+# Create occasions
+occasion = Occasion.new
+occasion.occasion_name = 'first date'
+occasion.save
+
+occasion = Occasion.new
+occasion.occasion_name = 'job interview'
+occasion.save
+
+# Create Outfits
+outfit = Outfit.new
+outfit.user_id = '1'
+outfit.occasion_id = '2'
+outfit.rating_id = '1'
+outfit.photo = 'http://www.savantsourcing.com/wp-content/uploads/2014/12/interview-success.jpg'
+outfit.save
+
+outfit = Outfit.new
+outfit.user_id = '2'
+outfit.occasion_id = '1'
+outfit.rating_id = '2'
+outfit.photo = 'http://www.realmendrinkwhiskey.com/wp-content/uploads/2014/06/first-date.jpg'
+outfit.save
+
+# Create rating
+rating = Rating.new
+rating.outfit_id = '1'
+rating.positive = '2'
+rating.negative = '3'
+rating.save
+
+rating = Rating.new
+rating.outfit_id = '2'
+rating.positive = '5'
+rating.negative = '2'
+rating.save
