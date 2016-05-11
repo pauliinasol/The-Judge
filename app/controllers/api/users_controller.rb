0bf110a56a671f05fed614d658942541bdc6565f
florianjosefreheis <florianjosefreheis@gmail.com>
2016-05-11 14:30:16 +1000

    bug fixes routes

--- app/controllers/api/users_controller.rb
+++ app/controllers/api/users_controller.rb
@@ -2,6 +2,7 @@ module Api
   class UsersController < ApplicationController
     before_filter :authenticate_user!, except: [:create, :index]
 
+
     # validates_uniqueness_of :email, message: "Sorry an account with that email already exists"
     # validates_presence_of :user_name
 
@@ -21,6 +22,31 @@ module Api
       user.email = params[:email]
       user.password_digest = params[:password]
       if user.save
+        render json: user.to_json, status: 201
+      else
+        render json: user.errors.to_json
+      end
+    end
+
+    def current_user
+    User.find_by(id: session[:user_id])
+    end
+
+    def logged_in?
+      if current_user
+        return true
+      else
+        return false
+      end
+    end
+
+    def login
+      User.find_by(email: params[:email])
+
+      if user && user.authenticate(params[:password])
+
+        session[:user_id] = user.id
+
         render json: user.to_json, status: 201
       else
         render json: user.errors.to_json
--- config/routes.rb
+++ config/routes.rb
@@ -1,21 +1,21 @@
 Rails.application.routes.draw do
 
   get '/api/users' => 'api/users#index'
-  get 'api/users/:id' => 'api/user#show'
+  get '/api/users/:id' => 'api/users#show'
   post '/api/users' => 'api/users#create'
-  get '/api/users/login'  => 'api/users#login'
+  post '/api/users/login' => 'api/users#login'
 
   get '/api/outfits' => 'api/outfits#index'
-  get 'api/outfits/:id' => 'api/outfits#show'
+  get '/api/outfits/:id' => 'api/outfits#show'
   post '/api/outfits' => 'api/outfits#create'
-  post 'api/outfits/photo' => 'api/outfits#'
+  post '/api/outfits/photo' => 'api/outfits#upload'
 
   get '/api/ratings' => 'api/ratings#index'
-  get 'api/ratings/:id' => 'api/ratings#show'
+  get '/api/ratings/:id' => 'api/ratings#show'
   post '/api/ratings' => 'api/ratings#create'
 
   get '/api/occasions' => 'api/occasions#index'
-  get 'api/occasions/:id' => 'api/occasions#show'
+  get '/api/occasions/:id' => 'api/occasions#show'
   post '/api/occasions' => 'api/occasions#create'
 
   resource :outfits
